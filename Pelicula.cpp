5a76d723400a198d2d9ac61c868c12f3e531eec7
jdonairee <javidonairepin@gmail.com>
2026-06-14 13:37:51 -0600

    Cambios en Interfaces

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -1,2 +1,2 @@
 #include "Pelicula.h"
-Pelicula::Pelicula(string titulo, string genero,)
+Pelicula::Pelicula(string titulo, string genero,int anio, string clasificacion,double duracion, string director): Contenido (titulo, genero, duracion){}
