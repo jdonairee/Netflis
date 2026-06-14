3c011c79a64dc9d03ecdf8ea3f6e4ac7b7ef7e2e
Miguel-Jahen <a01425106@tec.mx>
2026-06-14 14:48:43 -0600

    Update Contenido.cpp

--- Contenido.cpp
+++ Contenido.cpp
@@ -1,1 +1,30 @@
 #include "Contenido.h"
+
+Contenido::Contenido(){
+  id = 0;
+  titulo = "";
+  genero = "";
+  anio = 0;
+  clasificacion = "";
+  duracion = 0;
+}
+
+Contenido::Contenido(int _id, string _titulo, string _genero, int _anio, string _clasificacion, double _duracion) {
+    id = _id;
+    titulo = _titulo;
+    genero = _genero;
+    anio = _anio;
+    clasificacion = _clasificacion;
+    duracion = _duracion;
+}
+
+string Contenido::getTitulo() {
+    return titulo;
+}
+
+int Contenido::getId() {
+    return id;
+}
+
+Contenido::~Contenido() {
+}
