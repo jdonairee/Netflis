2c2c6a8da0c4c9e1f8ca85217b24e3894d32950d
MontserratNavarrete <A01425079@tec.mx>
2026-06-14 17:00:47 -0600

    Update Plataforma.h

--- Plataforma.h
+++ Plataforma.h
@@ -1,8 +1,20 @@
 #ifndef PLATAFORMA_H
 #define PLATAFORMA_H
 #include <iostream>
+#include <string>
+#include "Catalogo.h"
+#include "GestorCuenta.h"
+#include "Contenido.h"
 using namespace std;
-class Plataforma{
 
+class Plataforma{
+private:
+    string nombre;
+    Catalogo catalogo;
+    GestorCuenta gestor;
+public:
+    Plataforma(string _nombre);
+
+    void agregarContenido(Contenido* c);
 };
 #endif
