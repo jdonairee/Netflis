314dcf84a18fa9da2207980d24196a40d3b9c8c8
MontserratNavarrete <A01425079@tec.mx>
2026-06-14 14:34:40 -0600

    Update Evento.cpp

--- Evento.cpp
+++ Evento.cpp
@@ -1,1 +1,48 @@
 #include "Evento.h"
+
+Evento::Evento(int _ID, string _titulo, string _genero, int _anio, string _clasificacion, double _duracion, string _fecha, string _hora) : Contenido(int _ID, string _titulo, string _genero, int _anio, string _clasificacion, double _duracion){
+    ID = _ID; 
+    titulo = _titulo;
+    genero = _genero; 
+    anio = _anio;
+    clasificacion = _clasificacion;
+    duracion = _duracion;
+    fecha = _fecha;
+    hora = _hora;
+    likes = 0;
+}
+
+void Evento::mostrarInfo(){
+    cout << "EVENTO" << endl
+         << "Titulo: " << titulo << endl
+         << "Género: " << genero << endl
+         << "Año: " << anio << endl
+         << "Clasificación: " << clasificacion << endl
+         << "Duración: " << duracion << endl
+         << "Hora: " << hora << endl
+         << "Fecha: " << fecha << endl
+         << "Likes: " << likes << endl;
+}
+
+double Evento::calcularDuracion(){
+    return _duracion;
+}
+
+
+void Evento::agregarALista(){
+    cout << titulo << " Agregado a mi lista." << endl;
+}
+
+void Evento::quitarDeLista(){
+    cout << titulo << " Eliminado de mi lista." << endl;
+}
+
+void Evento::darLike(){
+    likes += 1;
+}
+
+void Evento::quitarLike(){
+    if (likes > 0) {
+        likes -= 1;
+    }
+}
