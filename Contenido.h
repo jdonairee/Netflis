631c710189f25bc8719a44114c049c6ce2c64ab6
Miguel-Jahen <a01425106@tec.mx>
2026-06-14 14:44:45 -0600

    Update Contenido.h

--- Contenido.h
+++ Contenido.h
@@ -1,8 +1,31 @@
 #ifndef CONTENIDO_H
 #define CONTENIDO_H
-#include <iostream>
-using namespace std;
-class Contenido{
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Contenido {
+protected:
+    int id;
+    string titulo;
+    string genero;
+    int anio;
+    string clasificacion;
+    double duracion;
+
+public:
+    Contenido();
+    Contenido(int _id, string _titulo, string _genero, int _anio, string _clasificacion, double _duracion);
+
+    virtual void mostrarInfo() = 0;
+    virtual double calcularDuracion() = 0;
+
+    string getTitulo();
+    int getId();
+
+    virtual ~Contenido();
 };
+
 #endif
