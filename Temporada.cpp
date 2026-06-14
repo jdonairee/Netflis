da8aa6a8b5f4cfa4c93141096aba35f98d36f01a
jdonairee <javidonairepin@gmail.com>
2026-06-14 14:43:16 -0600

    Cambios en Clases

--- Temporada.cpp
+++ Temporada.cpp
@@ -1,1 +1,17 @@
 #include "Temporada.h"
+Temporada::Temporada(int numero):Episodio("",0){
+
+}
+double Temporada::calcularDuracion(){
+    double duracionTotal=0.0;
+    for(const auto& episodio:episodios){
+        duracionTotal+=episodio.calcularDuracion();
+    }
+    return duracionTotal;
+}
+void Temporada::agregarEpisodio(Episodio e){
+    episodios.push_back(e);
+}
+void Temporada::eliminarEpisodio(Episodio e){
+    
+}
--- Temporada.h
+++ Temporada.h
@@ -1,17 +1,20 @@
 #ifndef TEMPORADA_H
 #define TEMPORADA_H
 #include "Episodio.h"
-#include "ILikeable.h"
+#include <vector>
 #include <iostream>
 using namespace std;
-class Temporada:public Episodio,public ILikeable{
-private 
-    int num;
+class Temporada:public Episodio{
+private:
+    int numero;
     vector<Episodio> episodios;
 public:
-    Temporada(int num);
+    Temporada(int numero);
     double calcularDuracion();
-    void mostrarInfo();
+    void agregarEpisodio(Episodio e);
+    void eliminarEpisodio(Episodio e);
+    vector<Episodio> verEpisodios();
+    
 };
 
 #endif
