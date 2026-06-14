5f0a109c54674c1224f08b93e6cc050533240814
MontserratNavarrete <A01425079@tec.mx>
2026-06-14 14:29:51 -0600

    Update Evento.h

--- Evento.h
+++ Evento.h
@@ -1,8 +1,27 @@
 #ifndef EVENTO_H
 #define EVENTO_H
 #include <iostream>
+#include "Contenido.h"
+#include "IListable.h"
+#include "ILikeable.h"
 using namespace std;
-class Evento{
 
+class Evento : public Contenido, public IListable, public ILikeable{
+private:
+    string fecha;
+    string hora;
+    int likes;
+
+public:
+    Evento(string _fecha, string _hora) : Contenido(int _ID, string _titulo, string _genero, int _anio, string _clasificacion, double _duracion);
+
+    void mostrarInfo();
+    double calcularDuracion();
+
+    void agregarALista();
+    void quitarDeLista();
+
+    void darLike();
+    void quitarLike();
 };
 #endif
