43a1b934cf71a3934f51a512ca15ba0eb9c0eb34
MontserratNavarrete <A01425079@tec.mx>
2026-06-14 17:01:08 -0600

    Update Plataforma.cpp

--- Plataforma.cpp
+++ Plataforma.cpp
@@ -1,1 +1,9 @@
 #include "Plataforma.h"
+
+Plataforma::Plataforma(string _nombre){
+    _nombre = _nombre;
+}
+
+void Plataforma::agregarContenido(Contenido* c){
+    catalogo.agregarContenido(c);
+}
