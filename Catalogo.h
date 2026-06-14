025788a406d1acee41541c403fbf516667a4f2f0
Miguel-Jahen <a01425106@tec.mx>
2026-06-14 15:58:15 -0600

    Update Catalogo.h

--- Catalogo.h
+++ Catalogo.h
@@ -9,6 +9,24 @@
 using namespace std;
 
 class Catalogo{
+private:
+    vector<Contenido*> contenidos;
 
+public:
+    Catalogo();
+
+    vector<Contenido*> buscar(string titulo);
+    double calcularDuracionTotal();
+
+    void agregarContenido(Contenido* c);
+    void eliminarContenido(Contenido* c);
+
+    Catalogo operator+(Contenido* c);
+    Catalogo operator-(Contenido* c);
+
+    void mostrarCatalogo();
+
+    ~Catalogo();
 };
+
 #endif
