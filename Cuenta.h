1b0b9d45b691aa5591a3d44be583bccb333d98ff
MontserratNavarrete <A01425079@tec.mx>
2026-06-14 15:35:59 -0600

    Update Cuenta.h

--- Cuenta.h
+++ Cuenta.h
@@ -1,8 +1,26 @@
 #ifndef CUENTA_H
 #define CUENTA_H
-#include <iostream>
-using namespace std;
-class Cuenta{
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Contenido.h"
+using namespace std;
+
+class Cuenta{
+private:
+    string nombre;
+    string tipo;
+    vector <Contenido*>miLista;
+    vector <Contenido*>liked;
+public:
+    Cuenta(string _nombre, string _tipo);
+    void agregarAListaPersonal(Contenido* c);
+    void darLikeContenido(Contenido* c);
+
+    bool puedeVer(Contenido* c);
+
+    void verMiLista();
+    void verLikes();
 };
 #endif
