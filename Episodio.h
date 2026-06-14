4bfa4fa6fb25854c76cd164e4677014bc2cecdcf
MontserratNavarrete <A01425079@tec.mx>
2026-06-14 13:30:49 -0600

    Update Episodio.h

--- Episodio.h
+++ Episodio.h
@@ -1,8 +1,15 @@
 #ifndef EPISODIO_H
 #define EPISODIO_H
+
 #include <iostream>
 using namespace std;
-class Episodio{
 
+class Episodio{
+private:
+    string titulo;
+    double duracion;
+
+public: 
+    double calcularDuracion();
 };
 #endif
